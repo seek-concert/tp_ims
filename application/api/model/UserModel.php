389178bea50ba0fdc6a8e6fc5970a56ca6f69aea
1105420247@qq.com <zgl199589@>
2019-04-05 14:17:50 +0800

    api接口

---
+++ application/api/model/UserModel.php
@@ -0,0 +1,17 @@
+<?php
+// +----------------------------------------------------------------------
+// | 用户模型
+// +----------------------------------------------------------------------
+namespace app\api\model;
+
+use think\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'snake_user';
+    protected $field=true;
+    protected $type = [
+
+    ];
+
+}
