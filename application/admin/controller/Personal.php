3d056cb9eab38a9a74a5bf0c0b9b8d139e01eec9
mao1314 <849340505@qq.com>
2019-04-07 11:33:22 +0800

    提现管理

--- application/admin/controller/Personal.php
+++ application/admin/controller/Personal.php
@@ -11,6 +11,7 @@ namespace app\admin\controller;
 
 use app\admin\model\BunledModel;
 use app\admin\model\ConsumerLogModel;
+use app\admin\model\ExtractLogModel;
 use app\admin\model\PrepaidLogModel;
 use app\admin\model\ProductModel;
 use app\admin\model\UserModel;
@@ -290,6 +291,198 @@ class Personal extends Base
         return $this->fetch();
     }
 
+    /*
+     * 提现记录
+     */
+    public function extractlog()
+    {
+        if (request()->isAjax()) {
+            $param = input('param.');
+            $limit = $param['pageSize'];
+            $offset = ($param['pageNumber'] - 1) * $limit;
+            $uid = session('id');
+            $input_time_start = isset($param['input_time_start'])?strtotime($param['input_time_start']):'';
+            $input_time_end = isset($param['input_time_end'])?strtotime($param['input_time_end'].'23:59:59'):'';
+            $status = isset($param['status'])?$param['status']:'';
+            $number = isset($param['number'])?$param['number']:'';
+            $sqlmap = [];
+            //查询某个时间之后
+            if ($input_time_start != '' && $input_time_end == '') {
+                $sqlmap['input_time'] = ['gt', $input_time_start];
+            }
+            //查询某个时间之前
+            if ($input_time_start == '' && $input_time_end != '') {
+                $sqlmap['input_time'] = ['lt', $input_time_end];
+            }
+            //查询某个入库时间段
+            if($input_time_start != ''&& $input_time_end != ''){
+                $sqlmap['input_time'] = ['between',[$input_time_start,$input_time_end]];
+            }
+            //查询状态
+            if (!empty($status)) {
+                $sqlmap['status'] = ['eq', $status];
+            }
+            //查询订单号
+            if (!empty($number)) {
+                $sqlmap['number'] = ['eq', $number];
+            }
+            $sqlmap['user_id'] = ['eq',$uid];
+            $extractlog = new ExtractLogModel();
+            $user = new UserModel();
+            $selectResult = $extractlog->getExtractLogByWhere($sqlmap, $offset, $limit);
+
+            $status = config('extract_status');
+            // 拼装参数
+            foreach ($selectResult as $key => $vo) {
+                if(!empty($vo['update_time'])){
+                    $selectResult[$key]['update_time'] = date('Y-m-d H:i:s', $vo['update_time']);
+                }
+                $selectResult[$key]['input_time'] = date('Y-m-d H:i:s', $vo['input_time']);
+                $selectResult[$key]['user_id'] = $user->getOneRealName($vo['user_id']);
+//                if($vo['status'] == 1){
+//                    $selectResult[$key]['operate'] = showOperate($this->makeButton($vo['id'],4));
+//                }
+                $selectResult[$key]['status'] = $status[$vo['status']];
+            }
+            $return['total'] = $extractlog->getAllExtractLog($sqlmap);  //总数据
+            $return['rows'] = $selectResult;
+            return json($return);
+        }
+        return $this->fetch();
+    }
+
+    /*
+     * 提现验证管理
+     */
+    public function extractlog_management()
+    {
+        if (request()->isAjax()) {
+            $param = input('param.');
+            $limit = $param['pageSize'];
+            $offset = ($param['pageNumber'] - 1) * $limit;
+            $uid = session('id');
+            $input_time_start = isset($param['input_time_start'])?strtotime($param['input_time_start']):'';
+            $input_time_end = isset($param['input_time_end'])?strtotime($param['input_time_end'].'23:59:59'):'';
+            $status = isset($param['status'])?$param['status']:'';
+            $number = isset($param['number'])?$param['number']:'';
+            $sqlmap = [];
+            //查询某个时间之后
+            if ($input_time_start != '' && $input_time_end == '') {
+                $sqlmap['input_time'] = ['gt', $input_time_start];
+            }
+            //查询某个时间之前
+            if ($input_time_start == '' && $input_time_end != '') {
+                $sqlmap['input_time'] = ['lt', $input_time_end];
+            }
+            //查询某个入库时间段
+            if($input_time_start != ''&& $input_time_end != ''){
+                $sqlmap['input_time'] = ['between',[$input_time_start,$input_time_end]];
+            }
+            //查询状态
+            if (!empty($status)) {
+                $sqlmap['status'] = ['eq', $status];
+            }
+            //查询订单号
+            if (!empty($number)) {
+                $sqlmap['number'] = ['eq', $number];
+            }
+            $sqlmap['user_id'] = ['eq',$uid];
+            $extractlog = new ExtractLogModel();
+            $user = new UserModel();
+            $selectResult = $extractlog->getExtractLogByWhere($sqlmap, $offset, $limit);
+
+            $status = config('extract_status');
+            // 拼装参数
+            foreach ($selectResult as $key => $vo) {
+                if(!empty($vo['update_time'])){
+                    $selectResult[$key]['update_time'] = date('Y-m-d H:i:s', $vo['update_time']);
+                }
+                $selectResult[$key]['input_time'] = date('Y-m-d H:i:s', $vo['input_time']);
+                $selectResult[$key]['user_id'] = $user->getOneRealName($vo['user_id']);
+                if ($vo['status'] == 1){
+                    $selectResult[$key]['operate'] = showOperate($this->makeButton($vo['id'],4));
+                }else{
+                    $selectResult[$key]['operate'] = showOperate($this->makeButton($vo['id'],5));
+                }
+                $selectResult[$key]['status'] = $status[$vo['status']];
+            }
+            $return['total'] = $extractlog->getAllExtractLog($sqlmap);  //总数据
+            $return['rows'] = $selectResult;
+            return json($return);
+        }
+        return $this->fetch();
+    }
+
+    /*
+     * 提现验证
+     */
+    public function extractlog_verify()
+    {
+        if(request()->isPost()){
+            $param = input('param.');
+            $result = $this->validate($param, 'ExtractLogVerifyValidate');
+            if (true !== $result) {
+                // 验证失败 输出错误信息
+                return msg(-1, '', $result);
+            }
+            $param['update_time'] = time();
+            //开启事务
+            Db::startTrans();
+            try {
+                //获取提现记录信息
+                $extractlog = Db::name('extract_log')
+                    ->where(['id'=>$param['id']])
+                    ->find();
+                //修改提现记录表
+                $extractlog_update = Db::name('extract_log')
+                    ->where(['id'=>$param['id']])
+                    ->update($param);
+                if($param['status'] == 2){
+                    //提现成功 -- 清除冻结对应数值
+                    $user_detail_update = Db::name('user_detail')
+                        ->where(['uid'=>$extractlog['user_id']])
+                        ->setDec('funds',$extractlog['money']);
+                }else{
+                    //提现失败 -- 冻结的对应数值返回至余额
+                    $user_detail = Db::name('user_detail')
+                        ->where(['uid'=>$extractlog['user_id']])
+                        ->find();
+                    $funds = $user_detail['funds'] - $extractlog['money'];
+                    $balance = $user_detail['balance'] + $extractlog['money'];
+                    $user_detail_update = Db::name('user_detail')
+                        ->where(['uid'=>$extractlog['user_id']])
+                        ->setField(['funds'=>$funds,'balance'=>$balance]);
+                }
+                if ($extractlog_update && $user_detail_update) {
+                    // 提交事务
+                    Db::commit();
+                    return msg(1, url('personal/extractlog_management'), '验证成功');
+                } else {
+                    // 回滚事务
+                    Db::rollback();
+                    return msg(-1, '', '验证失败');
+                }
+            } catch (\Exception $e) {
+                // 回滚事务
+                Db::rollback();
+                return msg(-2, '', '验证失败');
+            }
+        }
+        $id = input('param.id');
+        $this->assign(['id' =>$id]);
+        return $this->fetch();
+    }
+
+    /*
+     * 删除提现记录
+     */
+    public function extractlog_del()
+    {
+        $id = input('param.id');
+        $extractlog = new ExtractLogModel();
+        $flag = $extractlog->delExtractLog($id);
+        return json(msg($flag['code'], $flag['data'], $flag['msg']));
+    }
 
     /**
      * 拼装操作按钮
@@ -337,8 +530,25 @@ class Personal extends Base
                     'icon' => 'fa fa-trash-o'
                 ]
             ];
+        }else if ($status == 4){
+            return [
+                '验证' => [
+                    'auth' => 'personal/extractlog_verify',
+                    'href' => url('personal/extractlog_verify', ['id' => $id]),
+                    'btnStyle' => 'primary',
+                    'icon' => 'fa fa-paste',
+                ]
+            ];
+        }else if($status == 5){
+            return [
+                '删除' => [
+                    'auth' => 'personal/extractlog_del',
+                    'href' => "javascript:extractlog_del(" .$id .")",
+                    'btnStyle' => 'danger',
+                    'icon' => 'fa fa-trash-o'
+                ]
+            ];
         }
-
     }
 
 }
